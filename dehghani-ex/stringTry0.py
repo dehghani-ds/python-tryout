895193002cda10de0a1d752579701df279ad55f1
dehghani <mr.dehghani@mydigipay.com>
2026-06-20 16:34:59 +0330

    chore: calculate length string

--- dehghani-ex/stringTry0.py
+++ dehghani-ex/stringTry0.py
@@ -11,3 +11,4 @@ print("my name is {1} and my age is {0}".format(name, age))
 print("my name is {name} and my age is {age}".format( age=age, name =name))
 
 print(f"my name is {name} and my age is {age}") # this is a format string
+print(len(name))
