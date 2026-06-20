a74299673ce0e02a0bbe670c3a7dfe150a0feb26
dehghani <mr.dehghani@mydigipay.com>
2026-06-20 16:32:21 +0330

    chore: format string

--- dehghani-ex/stringTry0.py
+++ dehghani-ex/stringTry0.py
@@ -4,3 +4,10 @@ print(name[::-2], age)
 print("""jadi
 mirmirani
 """)
+print(name.islower())
+
+# string format
+print("my name is {1} and my age is {0}".format(name, age))
+print("my name is {name} and my age is {age}".format( age=age, name =name))
+
+print(f"my name is {name} and my age is {age}") # this is a format string
