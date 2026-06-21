dc001f92d6c606a11d230d343593d380b21dfcba
dehghani <mr.dehghani@mydigipay.com>
2026-06-21 10:23:02 +0330

    chore: dictionary try out

---
+++ dehghani-ex/dictionaryWorkOut.py
@@ -0,0 +1,11 @@
+priceDictionary = {
+    'apple': 1500,
+    'banana': 2000,
+    'orange': 2500,
+    'cherry': [3000, 1000],
+}
+
+print(type(priceDictionary['cherry'].sort()))
+print(priceDictionary['cherry'])
+print(priceDictionary.values())
+print(priceDictionary.items())
