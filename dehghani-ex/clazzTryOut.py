ac13e879250ee20323a826aba2c3714f6ab4e08c
dehghani <mr.dehghani@mydigipay.com>
2026-07-06 20:57:49 +0330

    chore: check self in method class

---
+++ dehghani-ex/clazzTryOut.py
@@ -0,0 +1,12 @@
+name = 'alireza'
+class TiTop:
+    def walk():
+        print('Walking...')
+
+    def changeName():
+        name = 'lines'
+t = TiTop()
+TiTop.walk()
+print(name)
+TiTop.changeName()
+print(name)
