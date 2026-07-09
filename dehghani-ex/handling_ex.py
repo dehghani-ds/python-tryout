a7bf056f5ee6d473d00b1d64f8b9576f6a207c95
dehghani <mr.dehghani@mydigipay.com>
2026-07-09 12:38:02 +0330

    chore: work with exception

---
+++ dehghani-ex/handling_ex.py
@@ -0,0 +1,17 @@
+def divide(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print('division by zero')
+    return result
+num1,num2 = None, None
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+except ValueError:
+    print("input isn't a number")
+else:
+    print("Result:", divide(num1, num2))
+finally:
+    print("app ended successfully ;)")
